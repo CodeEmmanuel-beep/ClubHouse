0d6b400fef0ccebd47392a20f3dccaca5c4f13e6
Emmanuel <emmanuelchiedueke01@gmail.com>
2025-12-23 14:26:32 +0100

    modified profile_service, to connect my redis to render

--- app/services/profile_service.py
+++ app/services/profile_service.py
@@ -35,7 +35,7 @@ tracemalloc.start()
 
 
 logger = get_loggers("profile")
-redis_client = redis.Redis.from_url(settings.REDIS_URL)
+redis_client = redis.Redis.from_url(settings.REDIS_URL, ssl_cert_reqs=None)
 
 
 def caching(key: str):
